cae5a0971145c0e5ffd8f5e23da875f13c59f623
watahikaland <swao2022@gmail.com>
2026-01-13 14:41:17 +0900

    今の時間を教えてくれるプログラム

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello from GitHub Web Editor!")
+from datetime import datetime
+import pytz
+
+# 日本時間を取得
+tokyo_time = datetime.now(pytz.timezone('Asia/Tokyo'))
+print(f"現在の日本時間は {tokyo_time.strftime('%Y-%m-%d %H:%M:%S')} です！")
